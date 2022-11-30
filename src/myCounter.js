634d92e1aaf9aa85f2a0b1a8f53c89cb6d096216
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2022-11-30 14:10:52 -0300

    requisito 8 concluido

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var contar = 2; contar <= 3; contar += 1) {
+      myArray.push(contar);
     }
   }
   return myArray;
